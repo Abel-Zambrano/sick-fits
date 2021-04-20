6e117d42b7035597d1ba6055f99257ccba6df3f9
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2021-04-19 20:20:44 -0700

    update

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "525275d08c4528fb79a1";
+/******/ 	var hotCurrentHash = "ec7bf2bfd914eb43729e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
